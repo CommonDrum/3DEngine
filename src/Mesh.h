1aadba6d6e0873da57ad2bb8fd59f0faee030211
Karol Hetman <karol.hetman@protonmail.com>
2023-09-07 19:23:46 +0100

    Mesh

---
+++ src/Mesh.h
@@ -0,0 +1,20 @@
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <vector>
+#include "Shaders/Shader.h"
+
+class Mesh {
+    public:
+        // mesh data
+        std::vector<Vertex>       vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture>      textures;
+
+        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+        void Draw(Shader &shader);
+    private:
+        //  render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
+};  
