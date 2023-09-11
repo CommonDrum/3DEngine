8906004f2a87f5f8c38e0427fa691ffc7fc67a6d
Karol Hetman <karol.hetman@protonmail.com>
2023-09-11 23:56:57 +0100

    new

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,12 +1,2 @@
 #include "Mesh.h"
 
-struct Vertex {
-    glm::vec3 Position;
-    glm::vec3 Normal;
-    glm::vec2 TexCoords;
-};
-
-struct Texture {
-    unsigned int id;
-    std::string type;
-}; 
--- src/Mesh.h
+++ src/Mesh.h
@@ -3,6 +3,18 @@
 #include <vector>
 #include "Shaders/Shader.h"
 
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+}; 
+
 class Mesh {
     public:
         // mesh data
@@ -10,8 +22,6 @@ class Mesh {
         std::vector<unsigned int> indices;
         //std::vector<Texture>      textures;
 
-        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
-        void Draw(Shader &shader);
     private:
         //  render data
         unsigned int VAO, VBO, EBO;
